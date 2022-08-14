9ca731073c9ef53dba89ea182dbbb75a2fbff9df
Sebastian Mandrean <sebastian.mandrean@gmail.com>
2022-08-14 03:01:27 +0200

    test: Cover partial reoptimizations

--- tests/cucumber.rs
+++ tests/cucumber.rs
@@ -1,32 +1,62 @@
 use std::{
-    env, fs, io,
+    env, fmt, fs,
+    io::{Error as IoError, Write},
     path::PathBuf,
-    process::{Command, Stdio},
+    process::{Command, Output, Stdio},
 };
 
-use assert_cmd::Command as AssertCommand;
+use assert_cmd::{assert::OutputAssertExt, cargo::CargoError, Command as AssertCommand};
 use async_trait::async_trait;
 use cucumber::{given, then, when, World, WorldInit};
 use glob::glob;
 use itertools::Itertools;
 use path_absolutize::Absolutize;
+use petname::petname;
+use predicates::prelude::predicate;
+use thiserror::Error;
 
 const CARGO_CW_OPTIMIZOOR: &str = "cargo-cw-optimizoor";
 const CW_OPTIMIZOOR: &str = "cw-optimizoor";
 
-#[derive(Debug, WorldInit)]
+#[derive(Debug, Clone, WorldInit)]
 pub struct CwWorld {
     ws_root: PathBuf,
+    cmd_output: Option<Output>,
     artifacts: Vec<PathBuf>,
+}
+
+#[derive(Error, Debug)]
+pub enum CwWorldError {
+    Io(IoError),
+    Cargo(CargoError),
+}
+
+impl From<IoError> for CwWorldError {
+    fn from(source: IoError) -> Self {
+        CwWorldError::Io(source)
+    }
+}
+
+impl From<CargoError> for CwWorldError {
+    fn from(source: CargoError) -> Self {
+        CwWorldError::Cargo(source)
+    }
+}
+
+impl fmt::Display for CwWorldError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:?}", self)
+    }
 }
 
 #[async_trait(? Send)]
 impl World for CwWorld {
-    type Error = io::Error;
+    type Error = CwWorldError;
 
-    async fn new() -> io::Result<CwWorld> {
+    async fn new() -> Result<Self, Self::Error> {
         Ok(Self {
             ws_root: env::current_dir()?,
+            cmd_output: None,
             artifacts: vec![],
         })
     }
@@ -38,8 +68,14 @@ async fn is_in_workspace(world: &mut CwWorld, ws: String) -> anyhow::Result<()> 
     Ok(())
 }
 
-#[when(regex = r"the user runs cw-optimizoor\s?(for the first time|again)?")]
-async fn runs_cw_optimizoor(world: &mut CwWorld, cond: String) -> anyhow::Result<()> {
+#[when(
+    regex = r"the user\s?(successfully|unsuccessfully)? runs cw-optimizoor\s?(for the first time|again)?"
+)]
+async fn runs_cw_optimizoor(
+    world: &mut CwWorld,
+    result: String,
+    cond: String,
+) -> anyhow::Result<()> {
     if !cond.is_empty() && cond.ne("again") {
         let artifacts = world.ws_root.join("artifacts");
         if artifacts.is_dir() {
@@ -55,7 +91,31 @@ async fn runs_cw_optimizoor(world: &mut CwWorld, cond: String) -> anyhow::Result
     let mut cmd = AssertCommand::cargo_bin(CARGO_CW_OPTIMIZOOR)?;
     cmd.current_dir(world.ws_root.as_path());
     cmd.arg(CW_OPTIMIZOOR);
-    cmd.assert().success();
+
+    let assert = match result.as_str() {
+        "successfully" => cmd.assert().success(),
+        "unsuccessfully" => cmd.assert().failure(),
+        _ => unreachable!(),
+    };
+    world.cmd_output = Some(assert.get_output().clone());
+
+    Ok(())
+}
+
+#[given(expr = "the user makes a change in the {string} contract")]
+async fn makes_a_change_in_contract(world: &mut CwWorld, name: String) -> anyhow::Result<()> {
+    let filename = world
+        .ws_root
+        .join("contracts")
+        .join(name)
+        .join("src/lib.rs");
+    let mut file = fs::OpenOptions::new()
+        .write(true)
+        .append(true)
+        .open(filename)?;
+
+    let change = format!("pub fn {}() {{ assert!(true) }}", petname(1, ""));
+    writeln!(file, "{}", change)?;
 
     Ok(())
 }
@@ -100,6 +160,34 @@ async fn each_artifact_contains_str_function(
             entry.display()
         );
     }
+    Ok(())
+}
+
+#[then(expr = "only {string} is reoptimized")]
+async fn only_str_is_reoptimized(world: &mut CwWorld, name: String) -> anyhow::Result<()> {
+    // TODO: verify that the checksum changed
+    world
+        .cmd_output
+        .as_ref()
+        .expect("missing cmd output")
+        .clone()
+        .assert()
+        .stdout(predicate::str::contains(format!("{} was optimized", name)));
+
+    Ok(())
+}
+
+#[then(expr = "the other {int} are skipped")]
+async fn other_n_skipped(world: &mut CwWorld, n: usize) -> anyhow::Result<()> {
+    // TODO: verify that the checksum(s) didn't change
+    world
+        .cmd_output
+        .as_ref()
+        .expect("missing cmd output")
+        .clone()
+        .assert()
+        .stdout(predicate::str::contains("is unchanged. Skipping").count(n));
+
     Ok(())
 }
 
--- tests/features/optimizing_workspace.feature
+++ tests/features/optimizing_workspace.feature
@@ -2,12 +2,20 @@ Feature: Optimizing a CosmWasm workspace
 
   Scenario: Optimizes workspace for the first time
     Given the user is in the workspace "cw-plus"
-    When the user runs cw-optimizoor for the first time
+    When the user successfully runs cw-optimizoor for the first time
     Then 10 wasm files exist in the artifacts dir
     And each artifact contains a function named "execute"
 
   Scenario: Reoptimizes workspace a second time
     Given the user is in the workspace "cw-plus"
-    When the user runs cw-optimizoor again
+    When the user successfully runs cw-optimizoor again
     Then 10 wasm files exist in the artifacts dir
     And each artifact contains a function named "execute"
+
+  Scenario: Reoptimizes only changed contracts
+    Given the user is in the workspace "cw-plus"
+    And the user makes a change in the "cw1-subkeys" contract
+    When the user successfully runs cw-optimizoor again
+    Then 10 wasm files exist in the artifacts dir
+    But only "cw1_subkeys" is reoptimized
+    And the other 9 are skipped
